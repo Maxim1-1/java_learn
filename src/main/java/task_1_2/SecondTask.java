6c7f9f952ccb540221bfa8fe46d57e30673368ab
Maxim1-1 <kick52@yadnex.ru>
2023-10-16 11:26:12 +0300

    add 1.2 -task1

--- src/main/java/task_1_2/SecondTask.java
+++ src/main/java/task_1_2/SecondTask.java
@@ -4,17 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-//        Дан массив целых чисел.
-//                Необходимо реализовать метод, которые определяет, содержит ли данный массив дубликаты
-//
-//                Пример:
-//                Array: [4,5,6,6,8]
-//                Result: true
-//                Число 6 повторяется 2 раза
-//
-//                Array: [4,5,6,7,8]
-//                Result: false
-//                Дубликатов нет
 public class SecondTask {
 
     static int[] Array= {4,5,6,6,8};
